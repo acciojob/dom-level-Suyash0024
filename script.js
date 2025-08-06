d8193d527b11a47b85b46c68410af0391a173c9e
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-08-06 12:36:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 //your JS code here. If required.
 const element = document.getElementById("level");
 let level = 0;
-let current  = element;
 
-
-while(curent){
-	level++;
-	curent = current.parseElement;
+for (let current = element; current != null; current = current.parentElement) {
+  level++;
 }
 
 alert(`The level of the element is: ${level}`);
